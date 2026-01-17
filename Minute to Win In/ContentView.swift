d6c4aaefb2a5c4ec46eb62e32946bbc38a19575c
Isaac D2 <hi@isaacd2.com>
2026-01-17 18:52:56 -0500

    Initial Commit

---
+++ Minute to Win In/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minute to Win In
+//
+//  Created by Isaac D2 on 1/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Minute to Win In/Minute_to_Win_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minute_to_Win_InApp.swift
+//  Minute to Win In
+//
+//  Created by Isaac D2 on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Minute_to_Win_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
